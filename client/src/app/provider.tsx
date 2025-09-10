80e2ff6caf89be0a7a2c771d4af02e30f026a4ed
HNK-005 <huynhnamkha1010@gmail.com>
2025-09-10 22:23:41 +0700

    change file project

--- client/src/app/provider.tsx
+++ client/src/app/provider.tsx
@@ -5,8 +5,6 @@ import { ErrorBoundary } from 'react-error-boundary';
 import { HelmetProvider } from 'react-helmet-async';
 
 import { MainErrorFallback } from '@/components/errors/main';
-import { Notifications } from '@/components/ui/notifications';
-import { Spinner } from '@/components/ui/spinner';
 import { queryConfig } from '@/lib/react-query';
 
 type AppProviderProps = {
@@ -22,18 +20,12 @@ export const AppProvider = ({ children }: AppProviderProps) => {
   );
 
   return (
-    <React.Suspense
-      fallback={
-        <div className="flex h-screen w-screen items-center justify-center">
-          <Spinner size="xl" />
-        </div>
-      }
-    >
+    <React.Suspense fallback={<></>}>
       <ErrorBoundary FallbackComponent={MainErrorFallback}>
         <HelmetProvider>
           <QueryClientProvider client={queryClient}>
             {import.meta.env.DEV && <ReactQueryDevtools />}
-            <Notifications />
+            {children}
           </QueryClientProvider>
         </HelmetProvider>
       </ErrorBoundary>
